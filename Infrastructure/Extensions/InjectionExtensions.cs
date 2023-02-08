db96b12d0785f4b9bf86283b77654810acebfb1e
davael <dacisneros5@hotmail.com>
2023-02-08 16:00:56 -0300

    feat: add transient UOW

--- Infrastructure/Extensions/InjectionExtensions.cs
+++ Infrastructure/Extensions/InjectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Infrastructure.Persistences.Contexts;
+using Infrastructure.Persistences.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,6 +11,10 @@ namespace Infrastructure.Extensions
         public static IServiceCollection AddInjectionInfraestructure(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<ClubesContext>(options => options.UseSqlServer(configuration.GetConnectionString("Connection")));
+
+
+            services.AddTransient<IUnitOfWork, IUnitOfWork>();
+
             return services;
         }
     }
