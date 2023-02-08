91ab011def1341d24adb1de553823518d237553a
davael <dacisneros5@hotmail.com>
2023-02-08 15:42:15 -0300

    feat: add Paginations

---
+++ Infrastructure/Commons/Bases/BasePaginationRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace Infrastructure.Commons.Bases
+{
+    public class BasePaginationRequest
+    {
+        public int NumPage { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        private readonly int MaxPageSize = 50;
+        public string Order { get; set; } = "asc";
+        public string? Sort { get; set; } = null;
+
+        public int Records
+        {
+            get => PageSize;
+            set
+            {
+                PageSize = (value > MaxPageSize) ? MaxPageSize : value;
+            }
+        }
+    }
+}
