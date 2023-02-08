6d7597b1272fbf01afd377f5ad1358e23844f42d
davael <dacisneros5@hotmail.com>
2023-02-08 15:41:46 -0300

    feat: add Migrations

---
+++ Infrastructure/Migrations/20230208134756_InitDB2Tables.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitDB2Tables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Club",
+                columns: table => new
+                {
+                    ClubID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClubDes = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false, defaultValue: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Club", x => x.ClubID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Court",
+                columns: table => new
+                {
+                    CourtId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClubID = table.Column<int>(type: "int", nullable: false),
+                    CourtDes = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false, defaultValue: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Court", x => new { x.CourtId, x.ClubID });
+                    table.ForeignKey(
+                        name: "FK_Court_Club_ClubID",
+                        column: x => x.ClubID,
+                        principalTable: "Club",
+                        principalColumn: "ClubID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Court_ClubID",
+                table: "Court",
+                column: "ClubID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Court");
+
+            migrationBuilder.DropTable(
+                name: "Club");
+        }
+    }
+}
