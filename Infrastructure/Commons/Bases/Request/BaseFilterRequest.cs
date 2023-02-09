ca5285eae324c417ffd5e2f790eb8b9ac301f2e3
Damian Cisneros <dacisneros5@hotmail.com>
2023-02-08 22:49:14 -0300

    chore: remove using

--- Infrastructure/Commons/Bases/Request/BaseFilterRequest.cs
+++ Infrastructure/Commons/Bases/Request/BaseFilterRequest.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Infrastructure.Commons.Bases.Request
+﻿namespace Infrastructure.Commons.Bases.Request
 {
     public class BaseFilterRequest : BasePaginationRequest
     {
--- Infrastructure/Persistences/Interfaces/IClubRepository.cs
+++ Infrastructure/Persistences/Interfaces/IClubRepository.cs
@@ -1,11 +1,6 @@
 ﻿using Domain.Entities;
 using Infrastructure.Commons.Bases.Request;
 using Infrastructure.Commons.Bases.Response;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Persistences.Interfaces
 {
