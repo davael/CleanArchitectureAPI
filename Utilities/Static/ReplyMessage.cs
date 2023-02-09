94ffa2ea9914abcc638eb2fdf699ca321be60169
davael <dacisneros5@hotmail.com>
2023-02-09 13:32:19 -0300

    feat: add utilities

---
+++ Utilities/Static/ReplyMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace Utilities.Static
+{
+    public  class ReplyMessage
+    {
+        public const string MESSAGE_QUERY = "Consulta exitosa.";
+        public const string MESSAGE_QUERY_EMPTY = "No se encontraron registros.";
+        public const string MESSAGE_SAVE = "Se registros correctamente.";
+        public const string MESSAGE_EXISTS = "Se registros ya existe.";
+        public const string MESSAGE_ACTIVATE = "El registro ha sido activado.";
+        public const string MESSAGE_VALIDATE = "Hay errores de validacion.";
+    }
+}
