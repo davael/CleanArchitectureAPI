d6f8817029899d69c80785112f1022ce373f7b49
davael <dacisneros5@hotmail.com>
2023-02-09 13:32:42 -0300

    feat: add DTOS and validator

---
+++ Application/Dtos/Request/ClubRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos.Request
+{
+    public class ClubRequestDto
+    {
+        public string ClubDes { get; set; }
+        public bool Active { get; set; }
+    }
+}
---
+++ Application/Validators/Club/ClubValidator.cs
@@ -0,0 +1,14 @@
+﻿using Application.Dtos.Request;
+using FluentValidation;
+
+namespace Application.Validators.Club
+{
+    public class ClubValidator: AbstractValidator<ClubRequestDto>
+    {
+        public ClubValidator() {
+            RuleFor(x => x.ClubDes)
+                .NotNull().WithMessage("El campo descripcion no puede ser nulo")
+                .NotEmpty().WithMessage("El campo no puede ser vacio");
+        }
+    }
+}
