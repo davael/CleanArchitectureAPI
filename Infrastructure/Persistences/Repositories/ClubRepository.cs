5cf20b41c97278d119021be2463670c5e83559b0
Damian Cisneros <dacisneros5@hotmail.com>
2023-02-08 22:44:49 -0300

    feat: implement repository Club

--- Infrastructure/Persistences/Repositories/ClubRepository.cs
+++ Infrastructure/Persistences/Repositories/ClubRepository.cs
@@ -1,21 +1,57 @@
 ﻿using Domain.Entities;
+using Infrastructure.Commons.Bases.Request;
+using Infrastructure.Commons.Bases.Response;
 using Infrastructure.Persistences.Contexts;
 using Infrastructure.Persistences.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Persistences.Repositories
 {
-    public class ClubRepository: GenericRepository<Club>, IClubRepository
+    public class ClubRepository : GenericRepository<Club>, IClubRepository
     {
         private readonly ClubesContext _context;
-        public ClubRepository( ClubesContext context)
+        public ClubRepository(ClubesContext context)
         {
             _context = context;
 
+        }
+        public async Task<BaseEntityResponse<Club>> ListClubes(BaseFilterRequest filters)
+        {
+            var response = new BaseEntityResponse<Club>();
+            var clubes = _context.Clubs.AsNoTracking().AsQueryable();
+
+            if(filters.NumFilter is not null && !string.IsNullOrEmpty(filters.TextFilter)) 
+            {
+                switch(filters.NumFilter)
+                {
+                    case 1:
+                        clubes= clubes.Where(x => x.ClubDes.Contains(filters.TextFilter)); break;
+                }
+            }
+            if(filters.StateFilter is not null)
+            {
+                clubes = clubes.Where(x => x.Active.Equals(filters.StateFilter));
+            }
+            response.TotalRecords = await clubes.CountAsync();
+            response.Items= await Ordering(filters,clubes).ToListAsync();
+            return response;
+        }
+
+        public Task<IEnumerable<Club>> ListSelectClubes()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<Club> GetClubById(int clubID)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> RegisterClub(Club club)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> EditClub(Club club)
+        {
+            throw new NotImplementedException();
         }
     }
 }
