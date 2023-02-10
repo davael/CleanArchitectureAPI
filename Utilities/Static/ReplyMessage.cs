85fd5e5ed9245c5cc9d71d531c534a6d7416794a
Damian Cisneros <dacisneros5@hotmail.com>
2023-02-10 19:08:14 -0300

    feat: add new message response

--- Utilities/Static/ReplyMessage.cs
+++ Utilities/Static/ReplyMessage.cs
@@ -8,5 +8,6 @@
         public const string MESSAGE_EXISTS = "Se registros ya existe.";
         public const string MESSAGE_ACTIVATE = "El registro ha sido activado.";
         public const string MESSAGE_VALIDATE = "Hay errores de validacion.";
+        public const string MESSAGE_FAILED = "Operacion fallida.";
     }
 }
