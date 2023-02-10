4215ea4c6a21c170c059c37f2d33a878b2a25db7
Damian Cisneros <dacisneros5@hotmail.com>
2023-02-10 19:08:35 -0300

    feat: create layer application in club

---
+++ Application/Interfaces/IClubApplication.cs
@@ -0,0 +1,20 @@
+﻿using Application.Commons.Bases;
+using Application.Dtos.Response;
+using Domain.Entities;
+using Infrastructure.Commons.Bases.Request;
+using Infrastructure.Commons.Bases.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IClubApplication
+    {
+        Task<BaseResponse<BaseEntityResponse<ClubResponseDto>>> ListClub(BaseFilterRequest filters);
+        Task<BaseResponse<IEnumerable<ClubSelectResponseDto>>> ListSelectClubs();
+        Task<BaseResponse<ClubResponseDto>> ClubById(int ClubId);
+    }
+}
---
+++ Application/Services/ClubApplication.cs
@@ -0,0 +1,44 @@
+﻿using Application.Commons.Bases;
+using Application.Dtos.Response;
+using Application.Interfaces;
+using Application.Validators.Club;
+using AutoMapper;
+using Infrastructure.Commons.Bases.Request;
+using Infrastructure.Commons.Bases.Response;
+using Infrastructure.Persistences.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class ClubApplication : IClubApplication
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly ClubValidator _validator;
+
+        public ClubApplication(IUnitOfWork unitOfWork, IMapper mapper, ClubValidator validator)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _validator = validator;
+        }
+        public Task<BaseResponse<ClubResponseDto>> ClubById(int ClubId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResponse<BaseEntityResponse<ClubResponseDto>>> ListClub(BaseFilterRequest filters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResponse<IEnumerable<ClubSelectResponseDto>>> ListSelectClubs()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
