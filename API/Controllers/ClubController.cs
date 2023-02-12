8c051c0bb615abad4161eb2a91f97bbeda519444
Damian Cisneros <dacisneros5@hotmail.com>
2023-02-11 21:56:55 -0300

    Feat: add club controller

---
+++ API/Controllers/ClubController.cs
@@ -0,0 +1,44 @@
+﻿using Application.Dtos.Request;
+using Application.Interfaces;
+using Infrastructure.Commons.Bases.Request;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClubController : ControllerBase
+    {
+        private readonly IClubApplication _clubApplication;
+        public ClubController(IClubApplication clubApplication)
+        {
+            _clubApplication = clubApplication;
+        }
+        [HttpPost]
+        public async Task<IActionResult> ListClubs([FromBody] BaseFilterRequest filters)
+        { 
+            var response = await _clubApplication.ListClub(filters);
+            return Ok(response);
+        }
+        [HttpGet("Select")]
+        public async Task<IActionResult> ListSelectClub()
+        {
+            var response = await _clubApplication.ListSelectClubs();
+            return Ok(response);
+        }
+        [HttpGet("{clubId:int}")]
+        public async Task<IActionResult> ClubById(int clubId)
+        {
+            var response = await _clubApplication.ClubById(clubId);
+            return Ok(response);
+        }
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterClub([FromBody] ClubRequestDto clubRequest)
+        {
+            var response= await _clubApplication.RegisterClub(clubRequest);
+            return Ok(response);
+        }
+        
+    }
+}
