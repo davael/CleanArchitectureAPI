214e8495ec2a2aa3c8e82684c30234d9128e0fb3
Damian Cisneros <dacisneros5@hotmail.com>
2023-02-11 21:57:40 -0300

    feat: add mapper club

---
+++ Application/Mappers/ClubMappingsProfile.cs
@@ -0,0 +1,23 @@
+﻿using Application.Dtos.Request;
+using Application.Dtos.Response;
+using AutoMapper;
+using Domain.Entities;
+using Infrastructure.Commons.Bases.Response;
+
+namespace Application.Mappers
+{
+    public class ClubMappingsProfile : Profile
+    {
+        public ClubMappingsProfile()
+        {
+            CreateMap<Club, ClubResponseDto>()
+                .ReverseMap();
+            CreateMap<BaseEntityResponse<Club>, BaseEntityResponse<ClubResponseDto>>()
+                .ReverseMap();
+            CreateMap<ClubRequestDto, Club>();
+            CreateMap<Club, ClubSelectResponseDto>()
+                .ReverseMap();
+
+        }
+    }
+}
